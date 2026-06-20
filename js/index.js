486e2ce2cfeec6dcf28be568c431d3569af84c19
LiMbi <ulululo9484@gmail.com>
2026-06-20 16:03:32 +0300

    Сделан функционал кнопкам

--- js/index.js
+++ js/index.js
@@ -1,27 +1,17 @@
+const { ipcRenderer } = require("electron");
 
+const close = document.getElementById("Close");
+const unwrap = document.getElementById("Unwrap");
+const Hide = document.getElementById("Hide");
 
+close.addEventListener("click", () => {
+    ipcRenderer.send("close-window");
+});
 
+Hide.addEventListener("click", () => {
+    ipcRenderer.send("minimize-window");
+});
 
-
-
-/*
-
-const input = document.getElementById("password");
-const button = document.getElementById("accept");
-
-button.addEventListener("click", () => {
-
-    if (password.value.length < 4) {
-        
-        alert("Пароль должен состоять больше 4 символов!");
-
-        return;
-
-    }
-
-    console.log("accept")
-    alert("Пароль зарегистрирован")
-
-})
-
-*/
+unwrap.addEventListener("click", () => {
+    ipcRenderer.send("toggle-window");
+});
--- main.js
+++ main.js
@@ -1,17 +1,37 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require("electron");
+
+let win;
 
 function createWindow() {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 850,
     height: 550,
-
     minWidth: 850,
     minHeight: 550,
-
     frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
   });
 
-  win.loadFile('html/index.html');
+  win.loadFile("html/index.html");
 }
+
+ipcMain.on("minimize-window", () => {
+  win.minimize();
+});
+
+ipcMain.on("close-window", () => {
+  win.close();
+});
+
+ipcMain.on("toggle-window", () => {
+  if (win.isMaximized()) {
+    win.unmaximize();
+  } else {
+    win.maximize();
+  }
+});
 
 app.whenReady().then(createWindow);
